bb7c6390af1bf6abcb7d4448ae3c23bf9654aa2d
michaelajames1911 <51917028+michaelajames1911@users.noreply.github.com>
2024-11-10 19:22:32 -0500

    adding the emotions dictionary

---
+++ emotions.py
@@ -0,0 +1,25 @@
+emotions_dict = {
+ #high energy, pleasant
+  "pleased": "feeling content and happy about a particular situation or person",
+  "focused": "engaged in only idea or emotion",
+  "upbeat": "feeling high energy and bubbly",
+  "product": "feeling like you are accomplishing your tasks or achieving your goals",
+  
+  #high energy, unpleasant
+  "tense": "unable to relax",
+  "frustrated": "upset because you cannot do something you want to do",
+  "overwhelmed": "feeling like you have been taken over by strong feelings",
+  "worried": "troubled about actual or potential problems",
+  
+  #low energy, pleasant
+  "calm": "feeling free of stress, agitation, and worry",
+  "compassionate": "showing care and concern for someone",
+  "grateful": "appreciative of something or someone",
+  "satisfied": "pleased with what you have or with something you did",
+  
+  #low energy, unpleasant
+  "sad": "feeling unhappy about something",
+  "burned out": "feeling exhausted from ongoing stress",
+  "vulnerable": "feeling like you could easily be emotionally or mentally hurt",
+  "disengaged": "feeling like you cannot focus or are disintered"
+}
